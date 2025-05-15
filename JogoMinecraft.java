d560586e36a052e7f3155b5e622353468dd56701
Erick Diego <diegoerick2006@gmail.com>
2025-05-14 22:05:51 -0300

    feat(p1): adiciona jogador construtor

--- JogoMinecraft.java
+++ JogoMinecraft.java
@@ -3,33 +3,48 @@ import java.util.Random;
 public class JogoMinecraft {
     public static void main(String[] args) throws Exception {
         var alex = new JogadorMinecraft("Alex");
+        var steve = new JogadorMinecraft("Steve Construtor");
 
         var gerador = new Random();
 
         while (true) {
-            if (gerador.nextInt(4) == 0) {
-                alex.levarDano();
-                if (!alex.estaVivo()) {
-                    System.out.println("GAME OVER");
-                    return;
+            if (alex.estaVivo()) {
+                tomarAcaoAleatoria(gerador, alex, new int[] { 1, 1, 1 });
+                if (gerador.nextInt(4) == 0) {
+                    alex.levarDano();
                 }
+                System.out.println(alex.toString());
             }
-            switch (gerador.nextInt(3)) {
-                case 0:
-                    alex.minerar();
-                    break;
-                case 1:
-                    alex.coletarMadeira();
-                    break;
-                case 2:
-                    alex.construir();
-                    break;
-                default:
-                    break;
+            if (steve.estaVivo()) {
+                tomarAcaoAleatoria(gerador, steve, new int[] { 1, 3, 6 });
+                if (gerador.nextInt(4) == 0) {
+                    steve.levarDano();
+                }
+                System.out.println(steve.toString());
             }
-            System.out.println(alex.toString());
+            if (!steve.estaVivo() && !alex.estaVivo()) {
+                System.out.println("GAME OVER");
+                return;
+            }
             System.out.println("\n============\n");
             Thread.sleep(5000);
+        }
+    }
+
+    public static void tomarAcaoAleatoria(Random gerador, JogadorMinecraft jogador, int[] pesos) {
+        int total = 0;
+
+        for (int i = 0; i < pesos.length; i++) {
+            total += pesos[i];
+        }
+
+        int valorSoteado = gerador.nextInt(total);
+        if (valorSoteado < pesos[0]) {
+            jogador.minerar();
+        } else if (valorSoteado < pesos[0] + pesos[1]) {
+            jogador.coletarMadeira();
+        } else if (valorSoteado < pesos[0] + pesos[1] + pesos[2]) {
+            jogador.construir();
         }
     }
 }
